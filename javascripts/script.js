ee3f3ded7ef6729f4ed3c5a20bda9448c95361ec
Olli K <gildean@salaliitto.com>
2013-05-25 15:52:22 +0300

    Change styles

---
+++ javascripts/script.js
@@ -0,0 +1,13 @@
+(function (D, window, undefined) {
+    var about = D.getElementById('about');
+    var aboutLink = D.getElementById('aboutlink');
+    aboutLink.onclick = function (e) {
+        e.preventDefault();
+        about.style.opacity = 1;
+    };
+    D.body.onclick = function (e) {
+        if (e.target.id !== 'about') {
+            about.style.opacity = 0;
+        }
+    };
+}(document, window));
